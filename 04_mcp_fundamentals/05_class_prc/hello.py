7904fce35b252333ecff8e1ca7601ade8bcc154b
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2025-09-07 18:16:09 +0500

    add name and description

--- 04_mcp_fundamentals/05_class_prc/hello.py
+++ 04_mcp_fundamentals/05_class_prc/hello.py
@@ -2,7 +2,7 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP(stateless_http=True)
 
-@mcp.prompt()
+@mcp.prompt(name="weather_instructions", description="Instructions for weather agent")
 def weather_agent_instructions(city:str):
     return f"""You are a helpful assistant that provides weather information of the {city}."""
 
